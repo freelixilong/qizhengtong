a7542e2c07c828c992f9cb597b3c85c11a1dd894
xlong <bluesky_14@qq.com>
2016-11-21 04:35:29 -0800

    finish the restAPI post basely

--- MySpider/pipelines.py
+++ MySpider/pipelines.py
@@ -26,34 +26,34 @@ class MyspiderPipeline(object):
     def close_spider(self, spider):
         #self.client.close()
     def getRestfulAPIData(item):
-    	url = self.server_uri + "/api/1.0/"
-    	json = {}
-    	json.mkey = item.depart
-    	json.subKey = item.section
-    	json.action = "post"
+    	url = self.server_uri + "/api/1.0"
+    	jsn = {}
+    	jsn.mkey = item.depart
+    	jsn.subKey = item.section
+    	jsn.action = "post"
     	if item.title:
     		data = {}
     	    data.title = item.title
     	    data.link = item.link
     	    data.append = item.append
     	    data.date = item.date
-    	    data.description = item.discription
+    	    data.desc = item.desc
+            jsn.data = data
 
-    	return (url, json)
+    	return (url, jsn)
 
     def process_item(self, item, spider):
 		try:
 			headers = {'content-type': 'application/json',}
-			(url, data) = self.getRestfulAPIData(item)
-			r = requests.post(url, param = {depart = item.depart,title = item.title}, timeout=1)  #will stuck here a little, need improving
-			r.json()
+			(url, jsn) = self.getRestfulAPIData(item)
+			r = requests.post(url, data = json.dump(jsn), headers = headers)  #will stuck here a little, need improving
 		except Exception, e:
 			raise DropItem("process_item the server(%s) response error" % self.server_uri)
 		else:
 			if r.status == '404':
-	        	return item
-	        else :
-
+                raise DropItem("process_item the server(%s) response error" % self.server_uri)
+	        else:
+                return item
 		finally:
 			pass
 	        
